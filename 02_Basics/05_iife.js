36a4f4b0452a5ed63559092bb4d4f2a3b163f6b7
varunbehere <varunbehere@gmail.com>
2024-02-13 15:27:32 +0530

    completed iife

--- 02_Basics/05_iife.js
+++ 02_Basics/05_iife.js
@@ -4,11 +4,22 @@
 // What is global scope pollution ?
 // There may exist many functions in the global scope of program and multiple variables may be present that may complicate program or pollute the program
 
+//after every iife there must exist a semicolon or it throws an error
 
 (function(num1,num2){
     num1+num2
-})()
+})();
 
 (function(name, last_name){
     console.log();
-})()
+})();
+
+//two types again 
+//named iife
+(function fun1(){
+    console.log("Function 1 named iife");
+})();
+//unnamed iife 
+((name)=>{
+    console.log(` My name is ${name}...`) // parameterised iife
+})('Varun');
