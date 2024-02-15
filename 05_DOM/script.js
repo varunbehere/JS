1a0c86baa71d797b5b152916a137a0175d1e37f4
varunbehere <varunbehere@gmail.com>
2024-02-15 16:51:48 +0530

    started DOM and learnt a little

---
+++ 05_DOM/script.js
@@ -0,0 +1,10 @@
+const listItem = document.querySelector('li')
+listItem.style.backgroundColor = 'green'
+
+const listItems = document.querySelectorAll('li')
+console.log(listItems)
+listItems[0].style.backgroundColor= 'rgb(195,225,253)'
+listItems[0].style.color= 'red'
+
+const uList = document.querySelector('ul')
+uList.style.listStyle= 'square'
