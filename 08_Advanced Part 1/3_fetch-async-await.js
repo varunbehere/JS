d4c650d3233ac43e2d9f587fa1930163e90309e5
varunbehere <varunbehere@gmail.com>
2024-03-13 18:17:24 +0530

    Done with fetch

--- 08_Advanced Part 1/3_fetch-async-await.js
+++ 08_Advanced Part 1/3_fetch-async-await.js
@@ -1,3 +1,12 @@
+// Fetch works on top of everything - that is it creates a special queue - priority queue / microtask queue
+// Operates in two modes 1)WebBrowser / Native Node 2)For reserving memory space  a)onFulfilled() b)onRejection()
+// Only network errors means rejection to fetch, Even HTTP error such as 404 also fullfill that is this will go to 
+// on fullfilled()
+
+
+// fetch (url, options)
+// options may contain - mode(GET,POST,..), credentials, etc...
+
 async function fetchU (){
     try {
         const data = await fetch('https://api.github.com/users')
