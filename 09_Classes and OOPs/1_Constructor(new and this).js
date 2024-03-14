fc75a814db759f0974ffacca52ea75b38195314e
varunbehere <varunbehere@gmail.com>
2024-03-14 18:40:37 +0530

    slight changes

--- 09_Classes and OOPs/1_Constructor(new and this).js
+++ 09_Classes and OOPs/1_Constructor(new and this).js
@@ -6,12 +6,11 @@ const obj1 = {
     totalLogin : 5
 }
 
-
 // Given Below is an Example of constructor in JS
 // Constructor - It is a special function that creates new instance of instance or an object
 // Constructor is called when it is used with the new keyword
 // Usually constructor is named with first letter as capital. It is not compulsion but most of the codebases have such naming convention.
-
+// Constructor doesnt require an explicit return, when new keyword is used it automatically returns the new Object.
 
 function UserStats(userName, age, totalLogin){
     // this keyword here denotes to the current instance 
@@ -19,13 +18,14 @@ function UserStats(userName, age, totalLogin){
     this.age = age
     this.totalLogin = totalLogin
 
-    return this    // we are returning the current instance
+    // return this    // we are returning the current instance
 }
 
-const var1 = new userStats('Varun', 19, 6)   // this new keyword overhere creates the new instance of  userStats() function 
-const var2 = new userStats('Behere',20, 10)
+const var1 = new UserStats('Varun', 19, 6)   // this new keyword overhere creates the new instance of  userStats() function 
+const var2 = new UserStats('Behere',20, 10)
 
 // If we dont use the new keyword in the above operations the values will get updated and we cant access the values in var1
-
-console.log(var1);
+console.log(var1)
 console.log(var2)
+
+console.log(var1.constructor) // this method is used to understand the relationship between object and constructor
